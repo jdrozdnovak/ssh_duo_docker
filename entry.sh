0f67047f28e9bf0ddcffd1958cf19980940f0a41
Jan Drozd Novak <jan.novak@outlook.com>
2023-10-28 12:43:57 +0000

    add entry file

---
+++ entry.sh
@@ -0,0 +1,12 @@
+adduser $SSH_USER --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
+mkdir -p /home/$SSH_USER/.ssh
+chmod -R go= /home/$SSH_USER/.ssh
+echo $PRIVATE_KEY >> /home/$SSH_USER/.ssh/id_ed25519
+chown -R anssvcuser:anssvcuser /home/$SSH_USER/.ssh
+chmod 600 anssvcuser:anssvcuser /home/$SSH_USER/.ssh/id_ed25519
+sed -i "s/^ikey =.*/ikey = $DUO_IKEY/" /etc/duo/login_duo.conf
+sed -i "s/^skey =.*/skey = $DUO_SKEY/" /etc/duo/login_duo.conf
+sed -i "s/^host =.*/host = $DUO_HOST/" /etc/duo/login_duo.conf
+sed -i "s/^#*AllowUsers .*/AllowUsers $SSH_USER/" /etc/ssh/sshd_config
+/usr/sbin/sshd -D
